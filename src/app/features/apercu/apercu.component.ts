2b33f5f931689fc6b8debb647ea091acaf2d6b63
SamEva-dev <sam.fokam@gmail.com>
2025-07-25 14:26:46 +0200

    update apercu

--- src/app/features/apercu/apercu.component.ts
+++ src/app/features/apercu/apercu.component.ts
@@ -1,24 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatIconModule } from "@angular/material/icon";
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { Card } from "primeng/card";
 import { CommonModule } from '@angular/common';
+import { Button, ButtonModule } from "primeng/button";
+import { ButtonGroupModule } from 'primeng/buttongroup';
 
 @Component({
   selector: 'app-apercu',
   imports: [
     CommonModule,
-    MatIconModule, 
-    MatFormFieldModule, 
-    TranslatePipe,],
+    MatIconModule,
+    MatFormFieldModule,
+    TranslatePipe,
+    ButtonModule,
+    ButtonGroupModule,
+    Button
+],
   templateUrl: './apercu.component.html',
   styleUrl: './apercu.component.scss'
 })
-export class ApercuComponent {
-constructor(translate: TranslateService) {}
+export class ApercuComponent implements OnInit {
+constructor(private  translate: TranslateService) {}
+  
 
  @Input() content: string = '';
   @Output() copy = new EventEmitter<void>();
   @Output() download = new EventEmitter<void>();
+
+  ngOnInit(): void {
+   
+    this.content= '# Your Dockerfile will appear here...';
+  }
 }
